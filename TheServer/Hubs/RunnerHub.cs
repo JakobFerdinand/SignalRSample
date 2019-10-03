9baf95ecf9e34548e31446b68ff54c0cc7052e6a
Jakob Ferdinand Wegenschimmel <jwegenschimmel@rzl.at>
2019-10-03 09:56:33 +0200

    build Server

---
+++ TheServer/Hubs/RunnerHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace TheServer.Hubs
+{
+    public class RunnerHub : Hub
+    {
+        public async Task RunnerCompleted(string id, TimeSpan runningTime)
+            => await Clients.All.SendAsync("RunnerCompletedMessage", id, runningTime);
+    }
+}
--- TheServer/Startup.cs
+++ TheServer/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TheServer.Hubs;
 
 namespace TheServer
 {
@@ -16,6 +17,7 @@ namespace TheServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +36,7 @@ namespace TheServer
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
+                endpoints.MapHub<RunnerHub>("/runnerHub");
             });
         }
     }
